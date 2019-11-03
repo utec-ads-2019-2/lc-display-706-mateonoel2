1472db0f78fa751ca68aed179ca2d53fbaefc476
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-11-02 19:30:17 -0500

    No se presentan errores en el debuguer

--- main.cpp
+++ main.cpp
@@ -12,10 +12,9 @@ void display(unt x, const string& y){
         else{
             for(unt i =0; i<x; i++) cout<<' ';
         }
-        cout<<' ';
-        if(c != *(--y.end())) cout<<' ';
+        cout<<"  ";
     }
-    cout<<'\n';
+    cout<<"\b\n";
     for(unt i = 0; i<x; i++){
         for(auto c : y){
             if(c=='0' or c=='4' or c=='5' or c=='6' or c=='8' or c=='9') cout<<'|';
@@ -23,9 +22,9 @@ void display(unt x, const string& y){
             for(unt j =0; j<x; j++) cout<<' ';
             if(c=='0' or c=='1' or c=='2' or c=='3' or c=='4' or c=='7' or c=='8' or c=='9') cout<<'|';
             else cout<<' ';
-            if(c != *(y.end()-1)) cout<< ' ';
+            cout<<' ';
         }
-        cout<<'\n';
+        cout<<"\b\n";
     }
     for(auto c : y){
         cout<<' ';
@@ -35,10 +34,10 @@ void display(unt x, const string& y){
         else{
             for(unt i =0; i<x; i++) cout<<' ';
         }
-        cout<<' ';
-        if(c != *(--y.end())) cout<<' ';
+        cout<<"  ";
     }
-    cout<<'\n';
+    cout<<"\b\n";
+
     for(unt i = 0; i<x; i++){
         for(auto c : y){
             if(c=='0' or c=='2' or c=='6' or c=='8') cout<<'|';
@@ -46,9 +45,9 @@ void display(unt x, const string& y){
             for(unt j =0; j<x; j++) cout<<' ';
             if(c=='0' or c=='1' or c=='3' or c=='4' or c=='5' or c=='6' or c=='7' or c=='8' or c=='9') cout<<'|';
             else cout<<' ';
-            if(c != *(y.end()-1)) cout<< ' ';
+            cout<<' ';
         }
-        cout<<'\n';
+        cout<<"\b\n";
     }
     for(auto c : y){
         cout<<' ';
@@ -58,18 +57,19 @@ void display(unt x, const string& y){
         else{
             for(unt i =0; i<x; i++) cout<<' ';
         }
-        cout<<' ';
-        if(c != *(--y.end())) cout<<' ';
+        cout<<"  ";
     }
+    cout<<"\b\n";
 }
 
 int main() {
     unt x;
     string y;
     while(cin>>x>>y){
+        if (x == 0 && y == "0")
+            break;
         display(x,y);
-        cout<<'\n';
-        cout<<'\n';
+        puts("");
     }
     return 0;
 }
